61cc2fa2d02aff174b31ab6a1fb3637878f876e2
Nicole Tibaldi <ntibaldi@gmail.com>
2016-06-01 17:18:58 -0400

    Scaling image

---
+++ frontend/components/NavBar.jsx
@@ -0,0 +1,48 @@
+var React = require('react');
+var Link = require('react-router').Link;
+var SessionStore = require('./../stores/session_store');
+var SessionApiUtil = require('./../util/session_api_util');
+
+var NavBar = React.createClass({
+  componentDidMount: function () {
+    SessionApiUtil.fetchCurrentUser();
+    SessionStore.addListener(this.forceUpdate.bind(this));
+  },
+
+  greeting: function () {
+    if (SessionStore.isUserLoggedIn()) {
+      return (
+        <nav>
+        <img className="favorite-logo h-button" src={favoriteUrl}/>
+        <Link to="/" className="h-button" onClick={SessionApiUtil.logout}>Log out</Link>
+        <p className="h-button">Hey, {SessionStore.currentUser().username}!</p>
+        </nav>
+      );
+    } else if (SessionStore.currentUserHasBeenFetched()) {
+      return (
+        <nav>
+        <Link to="/signup" className="h-button">Register</Link>
+        <Link to="/login" className="h-button sign-in">Sign in</Link>
+        </nav>
+      );
+    } else {
+      return (
+        <nav/>
+      )
+    }
+  },
+
+  render: function () {
+    return (
+      <div>
+        <header className="petsy-header">
+        <img className="logo" src={petsyUrl}/>
+          {this.greeting()}
+        </header>
+          {this.props.children}
+      </div>
+    );
+  }
+});
+
+module.exports = NavBar;
