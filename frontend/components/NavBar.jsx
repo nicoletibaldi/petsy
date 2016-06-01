a70220b9da30d92a967dcef9d14f97b1138e8e32
Nicole Tibaldi <ntibaldi@gmail.com>
2016-06-01 17:48:44 -0400

    Adds demo user

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -9,13 +9,18 @@ var NavBar = React.createClass({
     SessionStore.addListener(this.forceUpdate.bind(this));
   },
 
+  guestLogin: function(e) {
+    e.preventDefault();
+    SessionApiUtil.login({username: "Nicole", password: "password"})
+  },
+
   greeting: function () {
     if (SessionStore.isUserLoggedIn()) {
       return (
         <nav>
         <img className="favorite-logo h-button" src={favoriteUrl}/>
         <Link to="/" className="h-button" onClick={SessionApiUtil.logout}>Log out</Link>
-        <p className="h-button">Hey, {SessionStore.currentUser().username}!</p>
+        <a href="#" className="h-button">Hey, {SessionStore.currentUser().username}!</a>
         </nav>
       );
     } else if (SessionStore.currentUserHasBeenFetched()) {
@@ -23,6 +28,7 @@ var NavBar = React.createClass({
         <nav>
         <Link to="/signup" className="h-button">Register</Link>
         <Link to="/login" className="h-button sign-in">Sign in</Link>
+        <p className="h-button" onClick={this.guestLogin}>Demo User</p>
         </nav>
       );
     } else {
