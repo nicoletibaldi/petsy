53a811017645d0499c50ccde429ed74e6e1a10ea
Nicole Tibaldi <ntibaldi@gmail.com>
2016-06-01 15:28:37 -0400

    Favicon

--- frontend/stores/pet_store.js
+++ frontend/stores/pet_store.js
@@ -6,4 +6,48 @@ var PetStore = new Store(AppDispatcher);
 
 var _pets = {};
 
+
+
+var _dogs = [];
+
+PetStore.all = function () {
+  return Object.keys(_pets).map( function(pet))
+};
+
+PetStore.resetPets = function () {
+  _dogs = [];
+};
+
+var dogItem;
+
+PetStore.__onDispatch = function (payload) {
+  switch (payload.actionType) {
+    case PetConstants.PET_RECEIVED:
+      DogStore.resetDogs();
+      DogStore.resetOffset(payload.offset)
+      payload.dogs.forEach(function (dog) {
+        dogItem = {};
+        dogItem.id = dog.id.$t;
+        dogItem.name = dog.name.$t;
+        dogItem.age = dog.age.$t;
+        dogItem.size = dog.size.$t;
+        dogItem.sex = dog.sex.$t;
+        dogItem.breeds = dog.breeds.breed;
+        dogItem.city = dog.contact.city.$t;
+        dogItem.zipcode = dog.contact.zip.$t;
+        dogItem.email = dog.contact.email.$t;
+        if (dog.media.photos) {
+          dogItem.photos = dog.media.photos.photo;
+        }
+        dogItem.description = dog.description.$t;
+        _dogs.push(dogItem);
+      });
+      DogStore.__emitChange();
+
+
+      break;
+
+
+};
+
 module.exports = PetStore;
--- frontend/util/pet_api_util.js
+++ frontend/util/pet_api_util.js
@@ -1,18 +1,17 @@
 var PetActions = require('./../actions/pet_actions');
 
  module.exports = {
-   fetchPets: function () {
+   fetchRandomPet: function () {
      $.ajax({
        type: "GET",
        dataType: "JSONP",
        url: "http://api.petfinder.com/pet.getRandom?key=8702fd94d045acb703444d0f1969212c&format=json&output=full",
        success: function (result) {
-         debugger
+         PetActions.receiveSinglePet(result);
        },
        error: function () {
          console.log("Error");
        }
-
      });
    }
  };
