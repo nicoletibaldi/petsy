62b6c1caf2b8757e18eebb5a5b52c71e5c2c6422
ntibaldi92 <ntibaldi@gmail.com>
2016-06-02 11:27:24 -0400

    Adds pet index to homepage

--- frontend/components/PetIndexItem.jsx
+++ frontend/components/PetIndexItem.jsx
@@ -1,4 +1,27 @@
 var React = require('react');
 var Link = require('react-router').Link;
-var PetStore = require('./../stores/pet_store');
-var PetApiUtil = require('./../util/pet_api_util');
+
+var PetIndexItem = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  showDetail: function () {
+    // this.context.router.push(something to do with id);
+  },
+
+  render: function () {
+    //onClick of div (make it a link?) do this.showDetail
+    return(
+      <div className="pet-index-item">
+        <p>{this.props.pet.name.$t}</p>
+        <p>{this.props.pet.animal.$t}</p>
+        <p>{this.props.pet.sex.$t}</p>
+        <p>{this.props.pet.age.$t}</p>
+        <img src={this.props.pet.media.photos.photo[3].$t}/>
+      </div>
+    );
+  }
+});
+
+module.exports = PetIndexItem;
