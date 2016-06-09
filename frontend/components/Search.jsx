df2aaab07f57c68b68fa9d2e843817648d4c2ec3
Nicole Tibaldi <ntibaldi@gmail.com>
2016-06-09 15:47:01 -0400

    search bar does cool stuff

--- frontend/components/Search.jsx
+++ frontend/components/Search.jsx
@@ -3,14 +3,16 @@ var SearchStore = require('./../stores/search_store');
 var SearchApiUtil = require('./../util/search_api_util');
 var SearchActions = require('./../actions/search_actions');
 
-
 var Search = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
 
   getInitialState: function () {
-    return({query: ""});
+    return({
+      query: "",
+      expanded: false
+    });
   },
 
   componentDidMount: function () {
@@ -30,6 +32,14 @@ var Search = React.createClass({
     this.context.router.push("/search")
   },
 
+  expand: function () {
+    this.setState({expanded: true});
+  },
+
+  collapse: function () {
+    this.setState({expanded: false});
+  },
+
   queryChange: function (event) {
     var query = event.target.value;
     this.setState({query: query});
@@ -38,22 +48,29 @@ var Search = React.createClass({
     }
   },
 
+
   render: function () {
     var results = SearchStore.all()
     if (results.length > 10) {
       results = SearchStore.all().slice(0, 10);
     }
-    var resultLis = results.map(function (result) {
-      return(
-        <a href={"/#/pets/" + result.id} className="search-result" key={result.id}>{result.name}</a>
-      )
-    });
+
+    if (this.state.expanded) {
+      var resultLis = results.map(function (result) {
+        return(
+          <a href={"/#/pets/" + result.id} className="search-result" key={result.id}>{result.name}</a>
+        )
+      });
+    } else {
+      var resultLis = undefined;
+    }
     return(
-      <form className="search" onSubmit={this.handleSubmit}>
+      <form className="search" onSubmit={this.handleSubmit} onBlur={this.collapse}>
         <input className="search-input" type="text"
           placeholder="Describe your perfect pet!"
           value={this.state.query}
           onChange={this.queryChange}
+          onClick={this.expand}
         />
         <input className="search-button" value="Search" type="submit"/>
         <ul className="search-results">
