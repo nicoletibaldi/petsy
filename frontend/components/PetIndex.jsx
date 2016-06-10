f181c4e92c2b65b59b4bbf0be5e45efbddd65864
Stefan and Victor <Stefan_and_Victor@yahoo.com>
2016-06-10 11:43:47 -0400

    placeholders for empty pages

--- frontend/components/PetIndex.jsx
+++ frontend/components/PetIndex.jsx
@@ -46,9 +46,11 @@ var PetIndex = React.createClass({
   },
 
   render: function () {
-    if (!this.state.pets) {
+    if (this.state.pets.length < 1) {
       return (
-        <div>Fetching pets...</div>
+        <div>
+          <img src={noresultsUrl}/>
+        </div>
       );
     } else {
       return(
