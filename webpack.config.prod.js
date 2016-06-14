fbe80eccc2cbcd7f626c34a66d41cbda8c4f1012
ntibaldi92 <ntibaldi@gmail.com>
2016-06-14 17:37:34 -0400

    resolves react minification isseue

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: path.join(__dirname, 'frontend', 'petsy.jsx'),
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+   new webpack.DefinePlugin({
+     'process.env':{
+       'NODE_ENV': JSON.stringify('production')
+     }
+   }),
+   new webpack.optimize.UglifyJsPlugin({
+     compress:{
+       warnings: true
+     }
+   })
+ ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\node$/,
+        loader: "node-loader"
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
